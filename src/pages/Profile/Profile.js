59123f21a456842cdf68ef19ebdb9d951836612e
peichincc <104899687+peichincc@users.noreply.github.com>
2022-08-11 14:13:06 +0800

    SignUp API

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-import api from "../../utils/api";
-import getJwtToken from "../../utils/getJwtToken";
-
 const Wrapper = styled.div`
   padding: 60px 20px;
   display: flex;
@@ -11,7 +8,6 @@ const Wrapper = styled.div`
   align-items: center;
   text-align: center;
 `;
-
 const Title = styled.div`
   padding-bottom: 16px;
   border-bottom: 1px solid #979797;
@@ -19,57 +15,121 @@ const Title = styled.div`
   font-weight: bold;
 `;
 
-const Photo = styled.img`
-  margin-top: 24px;
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+`;
+const InputLabel = styled.label`
+  width: 110px;
+  line-height: 19px;
+  font-size: 16px;
+  color: #3f3a3a;
+  display: block;
+  margin-bottom: 10px;
+  @media screen and (max-width: 1279px) {
+    width: 100%;
+  }
+`;
+const InputControl = styled.input`
+  width: 250px;
+  height: 30px;
+  border-radius: 8px;
+  border: solid 1px #979797;
+  margin-bottom: 10px;
+  @media screen and (max-width: 1279px) {
+    margin-top: 10px;
+    width: 100%;
+  }
+`;
+const InputGroup = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 30px;
+  width: 400px;
+  @media screen and (max-width: 1279px) {
+    line-height: 17px;
+    font-size: 14px;
+    margin-top: 20px;
+    width: 100%;
+  }
 `;
 
-const Content = styled.div`
-  margin-top: 24px;
-`;
-
-const LogoutButton = styled.button`
+const SignUpButton = styled.button`
   margin-top: 24px;
 `;
 
 function Profile() {
-  const [profile, setProfile] = useState();
-
-  useEffect(() => {
-    async function getProfile() {
-      let jwtToken = window.localStorage.getItem("jwtToken");
-
-      if (!jwtToken) {
-        try {
-          jwtToken = await getJwtToken();
-        } catch (e) {
-          window.alert(e.message);
-          return;
-        }
-      }
-      window.localStorage.setItem("jwtToken", jwtToken);
-
-      const { data } = await api.getProfile(jwtToken);
-      setProfile(data);
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "name") {
+      setName(value);
     }
-    getProfile();
-  }, []);
+    if (name === "email") {
+      setEmail(value);
+    }
+    if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const handleSubmit = () => {
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+    };
+    console.log(data);
+    signUp(data);
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+  async function signUp(data) {
+    const response = await fetch(`https://kelvin-wu.site/api/1.0/user/signup`, {
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      method: "POST",
+    });
+    return await response.json();
+  }
 
   return (
     <Wrapper>
       <Title>會員基本資訊</Title>
-      <p>test</p>
-      {profile && (
-        <>
-          <Photo src={profile.picture} />
-          <Content>{profile.name}</Content>
-          <Content>{profile.email}</Content>
-          <LogoutButton
-            onClick={() => window.localStorage.removeItem("jwtToken")}
-          >
-            登出
-          </LogoutButton>
-        </>
-      )}
+      <form>
+        <Form>
+          <InputGroup>
+            <InputLabel>姓名</InputLabel>
+            <InputControl
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => handleInputChange(e)}
+            />
+            <InputLabel>信箱</InputLabel>
+            <InputControl
+              type="text"
+              name="email"
+              value={email}
+              onChange={(e) => handleInputChange(e)}
+            />
+            <InputLabel>密碼</InputLabel>
+            <InputControl
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </InputGroup>
+        </Form>
+      </form>
+      <SignUpButton onClick={() => handleSubmit()}>註冊</SignUpButton>
     </Wrapper>
   );
 }
